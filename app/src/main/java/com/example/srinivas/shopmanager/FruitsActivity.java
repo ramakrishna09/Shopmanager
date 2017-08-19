340f0823322025876140ca76abde34fa00caedf4
ramakrishna09 <ramakrishna09@users.noreply.github.com>
2017-08-19 13:08:45 +0530

    add new things

--- app/src/main/java/com/example/srinivas/shopmanager/FruitsActivity.java
+++ app/src/main/java/com/example/srinivas/shopmanager/FruitsActivity.java
@@ -13,31 +13,31 @@ import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
 
-public class ViewstoreActivity extends AppCompatActivity {
+public class FruitsActivity extends AppCompatActivity {
 
     private ListView listView;
     private DatabaseReference databaseReference;
-    private ArrayList<String> list = new ArrayList<>();
+    private ArrayList<String> list=new ArrayList<>();
     private ArrayAdapter<String> adapter;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_viewstore);
+        setContentView(R.layout.activity_fruits);
 
-        listView = (ListView) findViewById(R.id.listview);
-        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, list);
+        listView=(ListView)findViewById(R.id.listview);
+        adapter=new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,list);
         listView.setAdapter(adapter);
-
-        databaseReference = FirebaseDatabase.getInstance().getReferenceFromUrl("https://shopmanager-c94e7.firebaseio.com/Stores");
+        databaseReference= FirebaseDatabase.getInstance().getReferenceFromUrl("https://shopmanager-c94e7.firebaseio.com/Products/Fruits");
 
         databaseReference.addChildEventListener(new ChildEventListener() {
             @Override
             public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+
                 String value = dataSnapshot.getValue(String.class);
                 list.add(value);
                 adapter.notifyDataSetChanged();
+
             }
 
             @Override
@@ -47,6 +47,7 @@ public class ViewstoreActivity extends AppCompatActivity {
 
             @Override
             public void onChildRemoved(DataSnapshot dataSnapshot) {
+
                 String value = dataSnapshot.getValue(String.class);
                 list.remove(value);
                 adapter.notifyDataSetChanged();
@@ -63,7 +64,5 @@ public class ViewstoreActivity extends AppCompatActivity {
 
             }
         });
-
     }
 }
-
--- app/src/main/java/com/example/srinivas/shopmanager/VegitablesActivity.java
+++ app/src/main/java/com/example/srinivas/shopmanager/VegitablesActivity.java
@@ -13,31 +13,30 @@ import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
 
-public class ViewstoreActivity extends AppCompatActivity {
+public class VegitablesActivity extends AppCompatActivity {
 
     private ListView listView;
     private DatabaseReference databaseReference;
-    private ArrayList<String> list = new ArrayList<>();
+    private ArrayList<String> list=new ArrayList<>();
     private ArrayAdapter<String> adapter;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_viewstore);
+        setContentView(R.layout.activity_vegitables);
 
-        listView = (ListView) findViewById(R.id.listview);
-        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, list);
+        listView=(ListView)findViewById(R.id.listview);
+        adapter=new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,list);
         listView.setAdapter(adapter);
-
-        databaseReference = FirebaseDatabase.getInstance().getReferenceFromUrl("https://shopmanager-c94e7.firebaseio.com/Stores");
-
+        databaseReference= FirebaseDatabase.getInstance().getReferenceFromUrl("https://shopmanager-c94e7.firebaseio.com/Products/Vegitables");
         databaseReference.addChildEventListener(new ChildEventListener() {
             @Override
             public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+
                 String value = dataSnapshot.getValue(String.class);
                 list.add(value);
                 adapter.notifyDataSetChanged();
+
             }
 
             @Override
@@ -47,6 +46,7 @@ public class ViewstoreActivity extends AppCompatActivity {
 
             @Override
             public void onChildRemoved(DataSnapshot dataSnapshot) {
+
                 String value = dataSnapshot.getValue(String.class);
                 list.remove(value);
                 adapter.notifyDataSetChanged();
@@ -63,7 +63,7 @@ public class ViewstoreActivity extends AppCompatActivity {
 
             }
         });
-
     }
-}
 
+
+}
